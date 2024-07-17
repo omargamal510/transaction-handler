c24e06c85b9add61537265988b032e1a18cf21d2
omargamal510 <34138436+omargamal510@users.noreply.github.com>
2024-07-17 12:39:45 +0300

    [MODIFY] Table design and form of filter ... etc , add new font to the application

--- src/components/customersTable/CustomersTable.jsx
+++ src/components/customersTable/CustomersTable.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useMemo, useState } from "react";
 import getApi from "../../apis/getter";
 import GraphComponent from "../graphComponent/GraphComponent";
 
+
 let originalData = [];
 let originalTransactions = [];
 
@@ -51,12 +52,6 @@ export default function CustomersTable() {
       .reduce((acc, t) => acc + t.amount, 0);
   };
 
-  // const filteredAmounts = transactions.filter(
-  //   (t) => t.customer_id === Number(customer.id)
-  // );
-
-  // console.log(filteredAmounts);
-
   const filteredData = useMemo(() => {
     return data.filter((el) =>
       el.name.toLowerCase().includes(searchName.toLowerCase())
@@ -78,38 +73,43 @@ export default function CustomersTable() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Customers Table</h1>
+      {/* <h1 className="text-2xl font-bold mb-4 text-center">Customers Table</h1> */}
 
-      <div className="filter flex gap-x-3 text-black">
+      <form onSubmit={(e) => {e.preventDefault()}} className="filter flex gap-x-3 justify-center text-black min-w-100">
         <input
           value={searchName}
           onChange={handleNameChange}
           type="text"
           placeholder="filter by name"
+          className="w-full p-2 rounded-3xl text-lg outline-teal-500"
         />
         <input
           value={searchAmount}
           onChange={handleAmountChange}
           type="text"
           placeholder="filter by amount"
+          className="w-full p-2 rounded-3xl text-lg outline-teal-500"
         />
-      </div>
+      </form>
 
-      <div className="mb-4">
+
+      {/* <div className="mb-4">
         <h2>Total Customers: {data.length}</h2>
         <h2>Total Transactions: {transactions ? transactions.length : 0}</h2>
-      </div>
+      </div> */}
 
       {!filteredData || filteredData.length === 0 ? (
         <p className="text-gray-500">No customers found.</p>
       ) : (
-        <table className={`min-w-full bg-white border border-gray-300 rounded-lg shadow-md ${graphShow && 'hidden'}`}>
-          <thead>
-            <tr className="bg-teal-500 text-black">
-              <th className="py-3 px-4 text-left">ID</th>
+        <table className={`min-w-full my-10 bg-white rounded-3xl shadow-dark-grey overflow-hidden  ${graphShow && 'hidden'}`}>
+          <thead className="bg-teal-500 rounded-xl  text-black">
+            <tr className="text-black">
+              <th className="py-3 px-4 text-left">#ID</th>
               <th className="py-3 px-4 text-left">Name</th>
               <th className="py-3 px-4 text-left">Transactions Date</th>
               <th className="py-3 px-4 text-left">Transactions Amount</th>
+                            <th className="py-3 px-4 text-left">More Details</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -130,19 +130,19 @@ export default function CustomersTable() {
                   setSelectedName(customer.name);
                 }}
                 key={customer.id}
-                className="hover:bg-gray-100 text-black"
+                className="hover:bg-gray-100 cursor-pointer text-black"
               >
-                <td className="py-3 px-4 border-b border-gray-300">
+                <td className="font-bold py-3 px-4 border-b border-gray-300">
                   {customer.id}
                 </td>
-                <td className="py-3 px-4 border-b border-gray-300">
-                  {customer.name}
+                <td className="font-bold py-3 px-4 border-b border-gray-300">
+                <i className="fas fa-user text-teal-500"></i> {customer.name}
                 </td>
                 <td className="py-3 px-4 border-b border-gray-300">
                   {transactions
                     .filter((t) => t.customer_id === Number(customer.id))
                     .map((t) => (
-                      <p key={t.id}>{t.date}</p>
+                      <p key={t.id}><i className="fas fa-calendar text-teal-500"></i>  {t.date}</p>
                     ))}
                 </td>
                 <td className="py-3 px-4 border-b border-gray-300">
@@ -151,7 +151,10 @@ export default function CustomersTable() {
                     .map((t) => (
                       <p key={t.id}>{t.amount}</p>
                     ))}
-                  <strong>Total:</strong> {calcAmount(customer.id)}
+                    <div className="bg-teal-500 inline p-1 rounded text-white">Total: {calcAmount(customer.id)}</div>
+                </td> 
+                <td className="underline font-bold py-3 px-4 border-b border-gray-300">
+                  Click for more <i className="fa fa-angle-right text-teal-500"></i>
                 </td>
               </tr>
             ))}
@@ -160,6 +163,10 @@ export default function CustomersTable() {
       )}
 
       {graphShow && <GraphComponent amounts={sentAmounts} selectedName={selectedName} setGraphShow={setGraphShow} dates={sentDates} />}
+
+
+
+      
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
       backgroundImage: {
         "main-blue": "linear-gradient(to right, #38b2a0, #2c7a7b)", // Adjust colors as needed
       },
+      boxShadow: {
+        "dark-grey": "6px 4px 20px 1px rgba(0, 0, 0, 0.5)", // Customize the values as needed
+      },
     },
   },
   plugins: [],
